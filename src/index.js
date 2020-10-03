e409c6baab15923d232a4656d327b21af9c3c407
Vepsar <70876483+Vepsar@users.noreply.github.com>
2020-10-03 16:08:05 +0300

    feat:
    add max min avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+  var armin = 0;
+  if(typeof(array) != "undefined") {
+  array.forEach(element => {
+    if (armin > element) {
+      armin = element;
+    }
+});
+  return armin;
+  } else {return 0}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof(array) != "undefined") {
+  var armin = 0;
+  array.forEach(element => {
+    if (armin < element) {
+      armin = element;
+    }
+});
+  return armin;
+}  else {return 0}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof(array) != "undefined") {
+  if(array.length != 0) {
+  var count = array.length;
+  var aravg = array.reduce(function(sum,current) {
+     return sum+current
+  },0)
+
+return aravg/count;
+} else {return 0}} else {return 0}
 }
